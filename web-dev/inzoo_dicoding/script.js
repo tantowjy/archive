ff9f9d0398f512ae3782076092b4a9b510a89774
Tanto Wijaya <tanto.twjy@gmail.com>
2024-06-08 23:07:18 +0700

    Remove web-dev

--- web-dev/inzoo_dicoding/script.js
+++
@@ -1,8 +0,0 @@
-function menuFunction() {
-    var dropdown = document.getElementById("menu")
-    if (dropdown.className === "menu") {
-        dropdown.className += " responsive";
-    } else {
-        dropdown.className = "menu";
-    }
-}
--- web-dev/web_basic/js/main.js
+++
@@ -1,4 +0,0 @@
-const user_account = document.getElementById('user');
-
-const user_logged = "James";
-user_account.innerHTML = `Hai, ${user_logged}`;
